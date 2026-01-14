8caf9ac5ee94eadc57e5262fe99febcbc53068a4
mikeherron-aigt <mike.herron@aigt.art>
2026-01-14 11:54:20 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,16 +19,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         {children}
       </body>
     </html>
   );
 }
+
