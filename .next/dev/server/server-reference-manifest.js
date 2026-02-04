0923d9cbc40ef57a6ef3f49b50a5f55a631087e5
Jeff Ou <Jeff@space-jeff.local>
2026-02-04 01:04:24 -0800

    Ignore Next.js build artifacts (.next)

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tzXzqXpkgmEW/LRjLDv4RsH6qnUpL6ygFVj6E6PgPlw=\"\n}"
--- .next/dev/static/chunks/aigt_app_favicon_ico_mjs_ddb2e534._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/a900a_next_dist_05a639d0._.js"
-],
-    source: "dynamic"
-});
